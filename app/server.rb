72416c7b841b9f7c5cfad9f6d1bda223eeaa4620
immprada <im.mprada@gmail.com>
2025-02-02 00:16:21 -0500

    Bind to a port

--- app/server.rb
+++ app/server.rb
@@ -9,9 +9,9 @@ class YourRedisServer
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    # server = TCPServer.new(@port)
-    # client = server.accept
+    Uncomment this block to pass the first stage
+    server = TCPServer.new(@port)
+    client = server.accept
   end
 end
 
