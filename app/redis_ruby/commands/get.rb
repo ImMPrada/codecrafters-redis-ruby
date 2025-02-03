6fd02a7dba006f5650b61e7f610d2f9b8deb1c47
immprada <im.mprada@gmail.com>
2025-02-02 19:05:26 -0500

    add SET/GET new logic

--- app/redis_ruby/commands/get.rb
+++ app/redis_ruby/commands/get.rb
@@ -2,15 +2,30 @@ module RedisRuby
   module Commands
     class Get < Base
       def call(*args)
-        key = args.first
-        value = data_store[key]
-        resp_encoder(value)
+        @now = Time.now
+        @key, = args
+
+        respond
       end
 
       private
 
+      attr_reader :now, :key
+
+      def respond
+        var = data_store[key]
+        return nil_response if var.nil?
+        return nil_response if var[:xp] && now > var[:xp]
+
+        resp_encoder(var[:value])
+      end
+
       def resp_encoder(message)
         client.write("$#{message.bytesize}\r\n#{message}\r\n")
+      end
+
+      def nil_response
+        client.write("$-1\r\n")
       end
     end
   end
--- app/redis_ruby/commands/set.rb
+++ app/redis_ruby/commands/set.rb
@@ -2,15 +2,30 @@ module RedisRuby
   module Commands
     class Set < Base
       def call(*args)
-        key, value = args
-        data_store[key] = value
+        var = {}
+        @key, @value, @expiration_indicator, @expiration_value = args
+        add_expiration(var)
+
+        var[:value] = value
+
+        data_store[key] = var
+        puts("data_store: #{data_store}")
         client.puts(resp_encoder('+OK'))
       end
 
       private
 
+      attr_reader :key, :value, :expiration_indicator, :expiration_value
+
       def resp_encoder(message)
         "#{message}\r\n"
+      end
+
+      def add_expiration(var)
+        return unless expiration_indicator && expiration_value
+        raise "Unknown expiration indicator: #{expiration_indicator}" unless expiration_indicator == 'px'
+
+        var[:xp] = Time.now + (expiration_value.to_i / 1000.0)
       end
     end
   end
